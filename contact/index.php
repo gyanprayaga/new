1aee372be5014b2f356c575a166c3291afb75d92
Gyan <reachgyan@gmail.com>
2013-06-30 19:53:58 -0700

    JujubeRED
    files from red.jujubeweb.com

---
+++ contact/index.php
@@ -0,0 +1,35 @@
+<?php include '../header-first.php'; ?>
+<title>Jujube / Contact</title>
+<?php include '../header-second.php'; ?>
+            
+<section class="sub-banner">
+    <div class="holder">
+        <h1>Contact Us</h1>  
+        <p>We can take your website to new heights.</p>
+    </div>
+</section>    
+
+<section class="contact-form" id="contact">
+                <h3>Email</h3>
+                <p>You can always reach us by email at     
+    <script><!--
+var name = "contact";
+var domain = "jujubeweb.com";
+document.write('<a href=\"mailto:' + name + '@' + domain + '?Subject=Hello%2C%20Jujube%21' + '\">');
+document.write('contact@jujubeweb.com' + '</a>');
+// --></script>. You can expect a response within two or three days.
+</p>
+    
+    <h3>Project Planner</h3>
+    <p>We'll get back to you as soon as we can. <em>Please be nice - don't spam us and we won't spam you.</em></p>
+    <form>            
+          <p><input type="text" placeholder="Full Name" required></p>
+          <p><input type="email" placeholder="Email Address" required></p>
+          <p><textarea placeholder="What do you need done?" required></textarea></p>
+          <p><button type="submit" class="button primary alt contact-btn" ><i class="icon-hand-right"></i> Send!</button></p>
+    </form>
+
+</section>
+
+
+<?php include '../footer.php'; ?>
--- index.php
+++ index.php
@@ -2,59 +2,65 @@
         <title>Jujube</title>
          <?php include 'header-second.php'; ?>
 
-        <!--[if lt IE 7]>
-            <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
-        <![endif]-->
-
-            <!--<header>
-                <div class="logo"></div>
-                <nav>
-                    <a class="toggle-nav"></a>
-                    <ul>
-                        <li><a href="#">About</a></li> /
-                        <li><a href="#">Work</a></li> /
-                        <li><a href="#">Contact</a></li>
-                    </ul>
-                </nav>
-            </header>-->
-
             <section class="home-banner">      
               <div class="holder">
                 <h1>We are Jujube.</h1>
                 <p>We design digital media and craft beautiful websites.</p>
-                  <div class="btns"><p><a href="#down" class="button default"> <span class="icon-plus" aria-hidden="true"></span> Learn more</a><a href="#" class="button primary">Get in touch</a></p></div>
+                  <div class="btns"><p><a href="#down" class="button default scroll"> <span class="icon-plus" aria-hidden="true"></span> Learn more</a><a href="contact.php" class="button primary">Get in touch</a></p></div>
               </div>
             </section>
+
+
 
             <div class="divider" id="down"></div>            
 
             <section id="briefing">
-                <p>Nunc eu feugiat dolor. Integer non condimentum mi. Integer facilisis convallis porttitor. Interdum et malesuada fames ac ante ipsum primis in faucibus. <span>Praesent vel rutrum diam.</span></p>
+                <p>We are an LA-based <span class="red-light">design</span> & <span class="red-light">development</span> firm dedicated to building responsive, beautiful, rock-solid websites.</p>
             </section>
 
-            <section class="aspects">
-                <div class="aspect-image truck"><img src="img/truck.png" alt="A good-looking truck with square wheels."></div>
-                <h3>Form follows function.</h3>
-                <p>Nunc eu feugiat dolor. Integer non condimentum mi. Integer facilisis convallis porttitor. Interdum et malesuada fames ac ante ipsum primis in faucibus. Praesent vel rutrum diam.
-</span></p>
+            <section class="aspects" id="function">
+              <div class="container">
+                <div class="img truck"><img src="img/truck.png" alt="A nice truck with square wheels."></div>
+                <div class="content">
+                    <h3>Form follows function.</h3>
+                    <p>We emphasize functionality over design when building a product. We understand the importance of design that provides an enjoyable experience for the user without sacrificing functionality. A truck with square wheels may be pleasing to the eye but is impractical on the highway.</p>
+                    </p><p><small> <a href="http://fff.cmiscm.com/"><i class="icon-eye-open no-style"></i> &nbsp;Experience <em>"form follows function"</em></a></small></p>
+                </div>
+              </div>
             </section>
 
-            <section class="aspects">
-                <div class="aspect-image bonsai"><img src="img/bonsai.png" alt="Bonsai = simplicity."></div>
-                <h3>Keep it simple.</h3>
-                <p>Nunc eu feugiat dolor. Integer non condimentum mi. Integer facilisis convallis porttitor. Interdum et malesuada fames ac ante ipsum primis in faucibus. Praesent vel rutrum diam.</span></p>
+            <section class="aspects" id="simple">
+              <div class="container">
+                <div class="img bonsai"><img src="img/bonsai.png" alt="Bonsai = simplicity."></div>
+                <div class="content">
+                    <h3>Keep it simple.</h3>
+                    <p>We focus on developing web experiences that are elegant and intuitive. We keep designs to the functional minimum, thereby avoiding clutter and minimizing uncertainty for the user.</p><p><small> <a href="#redesigning-our-website" class="md-inline"><i class="icon-film no-style"></i> &nbsp;Watch <em>"Redesigning our website"</em></a></small></p>
+                </div>
+              </div>
             </section>
 
-            <section class="aspects">
-                <div class="aspect-image astronaut"><img src="img/astronaut.png" alt="Astronauts push the limits of technology and human capability."></div>                
-                <h3>Push the limits.</h3>
-                <p>Nunc eu feugiat dolor. Integer non condimentum mi. Integer facilisis convallis porttitor. Interdum et malesuada fames ac ante ipsum primis in faucibus. <span>Praesent vel rutrum diam.</span></p>
+            <section class="aspects" id="limits">
+              <div class="container">
+                <div class="img astronaut"><img src="img/astronaut.png" alt="Astronauts push the limits of technology and human capability."></div>                
+                <div class="content">                
+                    <h3>Push the limits.</h3>
+                    <p>We aren't afraid to try new things. We are constantly pushing the limits of our own imagination - exploring new possibilities in web design and development and striving to craft a better experience for the user. Our goal is not just to build beautiful websites, but ultimately to make the web a better place.</p>
+                    <p><small> <a href="blog/scalein"><i class="icon-book no-style"></i> &nbsp;Read about our latest project, <em>"ScaleIN"</em></a></small></p>
+                </div>
+              </div>
             </section>
 
-            <section class="contact">
+            <div class="contact-holder"><section class="contact" id="contact">
                 <h3>Interested?</h3>
                 <p>We could be a great fit for your next project.</p>
-                <p><a href="#" class="button default alt">See our work</a><a href="#" class="button primary alt">Get in touch!</a>
-            </section>
+                <p><a href="#" class="button default alt">See our work</a><a href="contact.php" class="button primary alt">Get in touch!</a>
+            </section></div>                        
+                                      
+            <div id="redesigning-our-website" class="j-modal mfp-hide">
+                <video controls>
+                    <source src="/vid/Jujube.mov">
+                        Your browser does not support HTML5 video.
+                </video> 
+            </div>                                      
 
         <?php include 'footer.php'; ?>
