878e546db0b3c918d81de23e7bc440ac4a5829c1
Gyan <reachgyan@gmail.com>
2013-06-30 19:41:30 -0700

    revisions

--- footer.php
+++ footer.php
@@ -1,20 +1,25 @@
-         <footer>
-            <p class="logo-left">
-               <a href="#" class="no-style"><span class="icon-jujube" aria-hidden="true"></span> Jujube         
-            </p>
-            <p class="text-right">
-                <script><!--
-var name = "contact";
-var domain = "jujubeweb.com";
-document.write('<a class="no-style" href=\"mailto:' + name + '@' + domain + '?Subject=Hello%2C%20Jujube%21' + '\">');
-document.write('mail' + '</a>');
-// --></script>
-                /  <a href="#" class="no-style">blog</a>  /  <a href="#" class="no-style">labs</a></br>&copy; Jujube 2013. All rights reserved. <span class="made">Made with <span class="icon-heart" aria-hidden="true"></span> in LA.</span>
-            </p>       
-         </footer>
+            <footer>
+                
+  <p class="logo-left">
+     <a href="#" class="no-style"><span class="icon-jujube" aria-hidden="true"></span> Jujube         
+  </p>
+  <p class="text-right">
+      <a href="#" class="no-style">mail</a>  /  <a href="#" class="no-style">blog</a>  /  <a href="#" class="no-style">labs</a></br>&copy; Jujube 2013. All rights reserved. <span class="made">Made with <span class="icon-heart" aria-hidden="true"></span> in LA.</span>
+  </p>       
+                    </div>
+            </footer>
 
-        <script src="//code.jquery.com/jquery.min.js"></script>
-        <script src="/js/vendor/jquery.magnific-popup.js"></script> 
-        <script src="/js/main.js"></script>
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+            var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+            (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+            g.src='//www.google-analytics.com/ga.js';
+            s.parentNode.insertBefore(g,s)}(document,'script'));
+        </script>
     </body>
 </html>
--- header-second.php
+++ header-second.php
@@ -1,40 +1,13 @@
         <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+        <meta name="viewport" content="width=device-width">
 
-        <link rel="stylesheet" href="/css/normalize.css">
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet/less" type="text/css" href="css/main.less" />
+        <script src="js/vendor/less.js"></script>
 
-        <link rel="stylesheet/less" type="text/css" href="/css/main.less" />
-        <script src="/js/vendor/less.js"></script>
-        
-        <script src="/js/vendor/modernizr-2.6.2.min.js"></script>
-        
-        <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
-            
-        <link rel="stylesheet" href="/css/magnific-popup.css"> 
+        <script src="js/vendor/modernizr-2.6.2.min.js"></script>
 
         <!--[if lte IE 7]><script src="js/lte-ie7.js"></script><![endif]-->
 
     </head>
     <body>
-        
-        <!--[if lt IE 8]>
-        <p class="old-browser">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
-        <![endif]-->
-        
-        <header>        
-            <div class="logo"><a href="/"><span class="icon-jujube" aria-hidden="true"></span> <span class="j-text">Jujube</span></a></div>
-                <nav class="main">
-                    
-                    <div class="togglers">
-                        <a class="toggle-nav no-style"><i class="icon-reorder"></i></a>
-                        <a class="close-nav no-style"><i class="icon-remove"></i></a>
-                    </div>
-                    
-                    <ul class="nav-elements">
-                        <li><a href="/search" class="search"><i class="icon-search"></i></a></li>
-                        <li><a href="/about">About</a></li>
-                        <li><a href="/work">Work</a></li>
-                        <li><a href="/contact">Contact</a></li>
-                    </ul>
-                </nav>
-        </header>
