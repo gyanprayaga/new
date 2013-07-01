e7c514e36b97fcf77a9c6b6ae0cd2d0986f7be51
Gyan <reachgyan@gmail.com>
2013-06-30 19:38:50 -0700

    New files
    Replaced old files with files on web (red.jujubeweb.com)

---
+++ search/index.php
@@ -0,0 +1,45 @@
+ <?php include '../header-first.php'; ?>
+<title>Jujube / Search</title>
+<?php include '../header-second.php'; ?>
+            
+<section class="home-banner no-resize">
+    <div class="holder" style="background-color:#167374">
+        <h1>Search</h1>  
+
+<form>
+  <p><input type="text" id="st-search-input" class="st-search-input" autofocus placeholder="Enter a search term..." /></p>
+</form>
+<script type="text/javascript">
+  var Swiftype = window.Swiftype || {};
+  (function() {
+    Swiftype.key = 'WezoLp9Q3UJ9Y3agzxhN';
+    Swiftype.inputElement = '#st-search-input';
+    Swiftype.resultContainingElement = '#st-results-container';
+    Swiftype.attachElement = '#st-search-input';
+    Swiftype.renderStyle = "inline";
+
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.async = true;
+    script.src = "//swiftype.com/embed.js";
+    var entry = document.getElementsByTagName('script')[0];
+    entry.parentNode.insertBefore(script, entry);
+  }());
+</script>        
+        
+        
+    </div>
+</section>
+
+
+
+<div class="divider" id="down"></div>
+
+<section id="briefing">
+   <div id="st-results-container"></div> 
+</section>
+
+       
+<?php include '../footer.php'; ?>
+
+
---
+++ work/index.php
@@ -0,0 +1,65 @@
+ <?php include '../header-first.php'; ?>
+<title>Jujube / Work</title>
+<?php include '../header-second.php'; ?>
+            
+<section class="sub-banner" style="background-image:url('http://cdn.londonandpartners.com/study/assets/49868-640x360-design-work-640.jpg')">
+    <div class="holder" style="background-color:rgba(192,44,25,0.7)">
+        <h1>Work</h1>  
+        <p>We work hard to build amazing products.</p>
+        <div class="eye"><i class="icon-eye-open"></i></div>
+    </div>
+</section> 
+
+<div class="divider" id="down"></div>
+
+<section id="briefing" class="center">
+    <p contenteditable="true">Jujube is a <span>small design firm</span> based in Los Angeles, California.</p>
+</section>
+
+<section class="about-parts" id="team">
+     <div class="container">
+          <div class="content">
+              <h2>Products</h2>
+              <div class="people">
+                  <div class="person gyan">
+                      <div class="snapshot"><img src="http://images1.wikia.nocookie.net/__cb20120429185058/reborn/images/5/50/Storm_Sunglasses_1.PNG" alt="Gyan's portrait"></div>                      
+                      <h3>Gyan</h3>
+                      <h6>Founder &amp; Developer</h6>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum metus massa, vehicula sit amet viverra in, ornare in dui.</p>
+                  </div>
+                  <div class="person jeev">
+                      <div class="snapshot"><img src="http://images.wikia.com/garoumush/images/3/38/Moros_Homid_Sunglasses_Sketch.png" alt="Jeev's portrait"></div>
+                      <h3>Jeev</h3>
+                      <h6>Designer &amp; Illustrator</h6>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum metus massa, vehicula sit amet viverra in, ornare in dui.</p>   
+                  </div>
+              </div>
+          </div>
+     </div>
+</section>
+
+<section class="about-parts" id="team">
+     <div class="container">
+          <div class="content">
+              <h2 class="last">Projects</h2>
+              <div class="people">
+                  <div class="person gyan">
+                      <div class="snapshot"><img src="http://images1.wikia.nocookie.net/__cb20120429185058/reborn/images/5/50/Storm_Sunglasses_1.PNG" alt="Gyan's portrait"></div>                      
+                      <h3>Gyan</h3>
+                      <h6>Founder &amp; Developer</h6>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum metus massa, vehicula sit amet viverra in, ornare in dui.</p>
+                  </div>
+                  <div class="person jeev">
+                      <div class="snapshot"><img src="http://images.wikia.com/garoumush/images/3/38/Moros_Homid_Sunglasses_Sketch.png" alt="Jeev's portrait"></div>
+                      <h3>Jeev</h3>
+                      <h6>Designer &amp; Illustrator</h6>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum metus massa, vehicula sit amet viverra in, ornare in dui.</p>   
+                  </div>
+              </div>
+          </div>
+     </div>
+</section>
+       
+<?php include '../footer.php'; ?>
+
+
